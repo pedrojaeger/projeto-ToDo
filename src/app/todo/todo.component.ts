fef6a4b42751a506be8fb3d58732da4b9d7ad626
jean cappellari <guijaeger@gmail.com>
2022-05-28 17:38:14 -0300

    primeiro commit do projeto, ja funcionando

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,47 @@
+import { ITask } from './../model/task';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+
+  todoForm !: FormGroup;
+  tasks : ITask [] = [];
+  inprogress : ITask [] = [];
+  done : ITask [] = [];
+
+  constructor(private fb : FormBuilder) { }
+
+  ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item : ["", Validators.required]
+    })
+  }
+
+  addTask() {
+    this.tasks.push({
+      description:this.todoForm.value.item,
+      done:false
+    })
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+
+}
